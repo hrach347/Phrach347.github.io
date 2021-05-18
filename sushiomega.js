7ae04ebca999d51f22240225e9db50c80f95bd22
hrach347 <80620675+hrach347@users.noreply.github.com>
2021-05-18 20:16:19 +0400

    Add files via upload

---
+++ sushiomega.js
@@ -0,0 +1,177 @@
+// JavaScript source code
+
+let s = 1
+function fila() {
+    a.innerHTML = s
+    s++
+    if (s > 51) {
+        s = 50
+        setTimeout(function () {
+            alert("You can't order more than 50 sushi's")
+        }, 400
+        )
+    }
+}
+function fila2() {
+    s = s - 1
+    a.innerHTML = s
+    if (s < 1) {
+        s = 1
+        setTimeout(function () {
+            alert("!!!")
+        }, 100
+        )
+    }
+}
+
+
+let s3 = 1
+function cali() {
+    a2.innerHTML = s3
+    s3++
+    if (s3 > 50) {
+        s3 = 50
+        setTimeout(function () {
+            alert("You can't order more than 50 sushi's")
+        }, 200
+        )
+    }
+}
+function cali2() {
+    s3 = s3 - 1
+    a2.innerHTML = s3
+    if (s3 < 1) {
+        s3 = 1
+        setTimeout(function () {
+            alert("!!!")
+        }, 100
+        )
+    }
+}
+
+
+
+let s4 = 1
+function rio() {
+    a3.innerHTML = s4
+    s4++
+    if (s4 > 50) {
+        s4 = 50
+        setTimeout(function () {
+            alert("You can't order more than 50 sushi's")
+        }, 200
+        )
+    }
+}
+function rio2() {
+    s4 = s4 - 1
+    a3.innerHTML = s4
+    if (s4 < 1) {
+        s4 = 1
+        setTimeout(function () {
+            alert("!!!")
+        }, 100
+        )
+    }
+}
+
+
+let s5 = 1
+function naz() {
+    a4.innerHTML = s5
+    s5++
+    if (s5 > 50) {
+        s5 = 50
+        setTimeout(function () {
+            alert("You can't order more than 50 sushi's")
+        }, 200
+        )
+    }
+}
+function naz2() {
+    s5 = s5 - 1
+    a4.innerHTML = s5
+    if (s5 < 1) {
+        s5 = 1
+        setTimeout(function () {
+            alert("!!!")
+        }, 100
+        )
+    }
+}
+
+let s6 = 1
+function tun() {
+    a5.innerHTML = s6
+    s6++
+    if (s6 > 50) {
+        s6 = 50
+        setTimeout(function () {
+            alert("You can't order more than 50 sushi's")
+        }, 200
+        )
+    }
+}
+function tun2() {
+    s6 = s6 - 1
+    a5.innerHTML = s6
+    if (s6 < 1) {
+        s6 = 1
+        setTimeout(function () {
+            alert("!!!")
+        }, 100
+        )
+    }
+}
+
+let s7 = 1
+function rain() {
+    a6.innerHTML = s7
+    s7++
+    if (s7 > 50) {
+        s7 = 50
+        setTimeout(function () {
+            alert("You can't order more than 50 sushi's")
+        }, 200
+        )
+    }
+}
+function rain2() {
+    s7 = s7 - 1
+    a6.innerHTML = s7
+    if (s7 < 1) {
+        s7 = 1
+        setTimeout(function () {
+            alert("!!!")
+        }, 100
+        )
+    }
+}
+
+let s8 = 1
+function eu() {
+    a7.innerHTML = s8
+    s8++
+    if (s8 > 50) {
+        s8 = 50
+        setTimeout(function () {
+            alert("You can't order more than 50 sushi's")
+        }, 200
+        )
+    }
+}
+function eu2() {
+    s8 = s8 - 1
+    a7.innerHTML = s8
+    if (s8 < 1) {
+        s8 = 1
+        setTimeout(function () {
+            alert("!!!")
+        }, 100
+        )
+    }
+}
+
+function trash() {
+    tra.innerHTML = s + s3 + s4 + s5 + s6 + s8 + s7 - 7
+}
